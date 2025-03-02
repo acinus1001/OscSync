842dcfaec661e28f21eb3326a18c948c73c7d547
kurovine9 <kurovine9@gmail.com>
2025-03-02 19:39:03 +0900

    feat: github link slash

---
+++ module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/slash/SlashGithubCommand.kt
@@ -0,0 +1,17 @@
+package dev.kuro9.application.discord.slash
+
+import dev.kuro9.internal.discord.slash.model.SlashCommandComponent
+import dev.minn.jda.ktx.coroutines.await
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.build.Commands.slash
+import org.springframework.stereotype.Component
+
+@Component
+class SlashGithubCommand : SlashCommandComponent {
+    override val commandData = slash("github", "return github url")
+
+    // todo 버튼식으로 바꿀까?
+    override suspend fun handleEvent(event: SlashCommandInteractionEvent) {
+        event.reply("https://github.com/KuroVine9/OscSync").await()
+    }
+}
