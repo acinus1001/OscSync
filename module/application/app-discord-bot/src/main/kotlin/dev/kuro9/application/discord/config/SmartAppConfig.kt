734efb80697b084533add5c746a62c095b0e894a
kurovine9 <kurovine9@gmail.com>
2025-03-03 11:22:45 +0900

    test: 테스트용 핸들러

--- module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/config/SmartAppConfig.kt
+++ module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/config/SmartAppConfig.kt
@@ -1,6 +1,9 @@
 package dev.kuro9.application.discord.config
 
+import dev.kuro9.common.logger.infoLog
 import dev.kuro9.internal.smartapp.api.model.SmartAppProperty
+import dev.kuro9.internal.smartapp.webhook.handler.SmartAppWebhookHandler
+import dev.kuro9.internal.smartapp.webhook.model.SmartAppWebhookResponse
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,4 +16,43 @@ class SmartAppConfig {
         object : SmartAppProperty {
             override val token: String = token
         }
+
+    @Bean
+    fun smartAppWebhookHandler(): SmartAppWebhookHandler = object : SmartAppWebhookHandler {
+        override fun onVerifyApp(appId: String, verificationUrl: String) {
+            infoLog("SmartAppWebhookHandler onVerifyApp $verificationUrl")
+        }
+
+        override fun onInitializePhase(
+            appId: String,
+            pageId: String,
+            prevPageId: String?
+        ): SmartAppWebhookResponse.ConfigurationData.InitData {
+            infoLog("SmartAppWebhookHandler onInitializePhase $pageId")
+            return SmartAppWebhookResponse.ConfigurationData.InitData(
+                name = "testname",
+                description = "testdescription",
+                id = "123",
+                permission = emptyList(),
+                firstPageId = "123"
+            )
+        }
+
+        override fun onPagePhase(
+            appId: String,
+            pageId: String,
+            prevPageId: String?
+        ): SmartAppWebhookResponse.ConfigurationData.PageData {
+            infoLog("SmartAppWebhookHandler onPagePhase $pageId")
+            return SmartAppWebhookResponse.ConfigurationData.PageData(
+                pageId = pageId,
+                nextPageId = null,
+                previousPageId = prevPageId,
+                complete = true,
+                name = "testname",
+                settings = emptyList(),
+            )
+        }
+
+    }
 }
