91145cb04b7b98e6cc8db359e907e76cce1308f7
이승찬 <kurovine9@gmail.com>
2025-03-26 22:49:35 +0900

    feat: global command update

--- module/internal/discord-client/src/main/kotlin/dev/kuro9/internal/discord/slash/SlashCommandListener.kt
+++ module/internal/discord-client/src/main/kotlin/dev/kuro9/internal/discord/slash/SlashCommandListener.kt
@@ -4,7 +4,11 @@ import dev.kuro9.common.logger.errorLog
 import dev.kuro9.common.logger.infoLog
 import dev.kuro9.internal.discord.model.DiscordEventHandler
 import dev.kuro9.internal.discord.slash.model.SlashCommandComponent
+import dev.minn.jda.ktx.coroutines.await
 import dev.minn.jda.ktx.interactions.commands.updateCommands
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import org.springframework.stereotype.Component
@@ -21,16 +25,22 @@ internal class SlashCommandListener(
         commandMap[event.name]?.handleEvent(event) ?: errorLog("No such command: ${event.fullCommandName}")
     }
 
-    override fun initialize(jda: JDA) = jda.run {
-        getGuildById(588993828309041153L)?.updateCommands {
-            addCommands(slashCommands.map { it.commandData })
-        }?.queue()
+    override fun initialize(jda: JDA): Unit = jda.run {
+        CoroutineScope(Dispatchers.IO).launch {
+            val job1 = launch {
+                getGuildById(588993828309041153L)?.updateCommands {
+                    addCommands(slashCommands.map { it.commandData })
+                }?.await()
+            }
+            val job2 = launch {
+                updateCommands {
+                    addCommands(slashCommands.map { it.commandData })
+                }.await()
+            }
 
-        infoLog("Commands initialized")
-
-        // global update
-//        updateCommands {
-//            addCommands(slashCommands.map { it.commandData })
-//        }.queue()
+            job1.join()
+            job2.join()
+            infoLog("Command Initialize Complete")
+        }
     }
 }
