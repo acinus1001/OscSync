c6f0b58bfac88c769a63b0f828e1d6f08892de12
이승찬 <kurovine9@gmail.com>
2025-03-31 13:52:42 +0900

    feat: exposed util add

--- module/domain/database/src/main/kotlin/dev/kuro9/domain/database/ExposedUtil.kt
+++ module/domain/database/src/main/kotlin/dev/kuro9/domain/database/ExposedUtil.kt
@@ -1,9 +1,16 @@
 package dev.kuro9.domain.database
 
-import org.jetbrains.exposed.sql.Between
-import org.jetbrains.exposed.sql.ExpressionWithColumnType
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.between
 
 infix fun <T : Comparable<T>, S : T?> ExpressionWithColumnType<in S>.between(range: ClosedRange<T>): Between {
     return this.between(range.start, range.endInclusive)
 }
+
+fun Query.fetchFirst() = this.limit(1).first()
+
+fun Query.fetchFirstOrNull() = this.limit(1).firstOrNull()
+
+fun Query.exists(): Boolean = Table.Dual.select(intLiteral(1))
+    .where(exists(this@exists))
+    .count() != 0L
