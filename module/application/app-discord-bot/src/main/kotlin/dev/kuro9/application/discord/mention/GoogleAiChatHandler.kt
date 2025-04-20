c7152fc876c21c2029c0d39820dc29e4f6cbb7c2
kurovine9 <kurovine9@gmail.com>
2025-04-20 22:10:28 +0900

    feat: 다른 사람 대화
    resolved #8

--- module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/mention/GoogleAiChatHandler.kt
+++ module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/mention/GoogleAiChatHandler.kt
@@ -148,7 +148,7 @@ class GoogleAiChatAbstractHandler(
                 info { "ref=${messageRef?.contentRaw}" }
                 val key = "${event.message.id}_${event.message.author.id}"
                 val refKey = messageRef?.let {
-                    "${it.id}_${event.message.author.id}"
+                    "${it.id}_${it.author.id}"
                 }
                 makeKey(key) to refKey?.let(::makeKey)
             }
