d71d9a20cd36eb8fe1b6d8edf580a4e5ba7391d9
이승찬 <kurovine9@gmail.com>
2025-10-28 14:38:57 +0900

    fix: 새 news 가져오지 못하는 문제 수정

--- module/application/app-general-batch/src/main/kotlin/dev/kuro9/application/batch/f1/news/tasklet/F1NewsFetchTasklet.kt
+++ module/application/app-general-batch/src/main/kotlin/dev/kuro9/application/batch/f1/news/tasklet/F1NewsFetchTasklet.kt
@@ -8,19 +8,16 @@ import kotlinx.coroutines.runBlocking
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.item.Chunk
 import org.springframework.batch.item.ExecutionContext
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.time.LocalDate
 
 @[StepScope Component]
 class F1NewsFetchTasklet(
     private val f1ParseService: F1NewsParseService,
     private val f1Service: F1NewsService,
-    @Value("#{jobParameters['executeDate']}") private val _executeDate: LocalDate,
 ) : ItemStreamIterableReaderWriter<F1NewsHtmlDto> {
 
     override fun readIterable(p0: ExecutionContext): Iterable<F1NewsHtmlDto> {
-        return runBlocking { f1ParseService.parseLatestNews().asReversed() }
+        return runBlocking { f1ParseService.parseLatestNews().asReversed() } // 시간순으로 예전 것부터
     }
 
     override fun write(p0: Chunk<out F1NewsHtmlDto>) {
