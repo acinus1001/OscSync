7ba1cbd6fa498c8f2f9c954fee6d53c8c40574c2
이승찬 <kurovine9@gmail.com>
2025-11-19 16:33:34 +0900

    feat: kotlinx datetime version 0.6 -> 0.7

--- multiplatform-common/date/src/commonMain/kotlin/dev/kuro9/multiplatform/common/date/util/DateTimeExtension.kt
+++ multiplatform-common/date/src/commonMain/kotlin/dev/kuro9/multiplatform/common/date/util/DateTimeExtension.kt
@@ -1,7 +1,10 @@
 package dev.kuro9.multiplatform.common.date.util
 
 import kotlinx.datetime.*
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 fun LocalDateTime.Companion.now(timeZone: TimeZone = TimeZone.of("Asia/Seoul")): LocalDateTime =
     Clock.System.now().toLocalDateTime(timeZone)
 
@@ -12,3 +15,13 @@ fun LocalDate.toRangeOfDay(): ClosedRange<LocalDateTime> = atTime(0, 0)..atTime(
 
 fun ClosedRange<LocalDate>.toDateTimeRange(): ClosedRange<LocalDateTime> =
     start.atTime(0, 0)..endInclusive.atTime(23, 59, 59, 59)
+
+fun ClosedRange<LocalDate>.toList(): List<LocalDate> {
+    val result = mutableListOf<LocalDate>()
+    var current = start
+    while (current <= endInclusive) {
+        result.add(current)
+        current = current.plus(1, DateTimeUnit.DAY)
+    }
+    return result
+}
