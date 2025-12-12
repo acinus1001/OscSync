0b4f3b96e34ddfa3655d241548bfa871436b5d24
이승찬 <kurovine9@gmail.com>
2025-12-12 11:36:21 +0900

    chore: 상태메시지 변경

--- module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/event/DiscordOnReadyEvent.kt
+++ module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/event/DiscordOnReadyEvent.kt
@@ -4,6 +4,8 @@ import dev.kuro9.common.logger.infoLog
 import dev.kuro9.domain.database.ExposedTestService
 import dev.kuro9.internal.discord.model.DiscordEventHandler
 import dev.minn.jda.ktx.coroutines.await
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.session.ReadyEvent
 import org.springframework.stereotype.Component
@@ -15,9 +17,11 @@ class DiscordOnReadyEvent(private val databaseTest: ExposedTestService) : Discor
     override suspend fun handle(event: ReadyEvent) {
         infoLog("discord client ready")
 
-        databaseTest.testDatabase()
+        withContext(Dispatchers.IO) {
+            databaseTest.testDatabase()
+        }
 
-        event.jda.presence.setPresence(Activity.customStatus("마작 관련 기능 마이그레이션 중. 문의 => @kurovine9"), true)
+        event.jda.presence.setPresence(Activity.customStatus("마작 관련 기능 마이그레이션 중. 문의 => /inquiry"), true)
         event.jda.selfUser.manager.setName("kurōnis automaton").await()
     }
 }
