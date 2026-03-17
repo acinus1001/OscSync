ec4cd6f3a35c057822e4b7ffe2007f179816b915
이승찬 <kurovine9@gmail.com>
2026-03-17 16:29:00 +0900

    fix: timezone +9 기준으로

--- module/application/app-general-batch/src/main/kotlin/dev/kuro9/application/batch/karaoke/scheduler/KaraokeCrawlJobScheduler.kt
+++ module/application/app-general-batch/src/main/kotlin/dev/kuro9/application/batch/karaoke/scheduler/KaraokeCrawlJobScheduler.kt
@@ -1,12 +1,14 @@
 package dev.kuro9.application.batch.karaoke.scheduler
 
+import dev.kuro9.multiplatform.common.date.util.now
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.toJavaLocalDate
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.configuration.JobRegistry
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.time.LocalDate
-import java.time.LocalTime
 
 @Component
 class KaraokeCrawlJobScheduler(
@@ -14,13 +16,13 @@ class KaraokeCrawlJobScheduler(
     private val jobRegistry: JobRegistry,
 ) {
 
-    @Scheduled(cron = "0 0 10,16 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 40 9,16 * * *", zone = "Asia/Seoul")
     fun runKaraokeJob() {
         val job = jobRegistry.getJob("karaokeCrawlJob")
         jobLauncher.run(
             job,
             JobParametersBuilder()
-                .addLocalDate("executeDate", LocalDate.now())
+                .addLocalDate("executeDate", LocalDate.now().toJavaLocalDate())
                 .addString("timeType", if (LocalTime.now().hour in 0..11) "AM" else "PM")
                 .toJobParameters()
         )
