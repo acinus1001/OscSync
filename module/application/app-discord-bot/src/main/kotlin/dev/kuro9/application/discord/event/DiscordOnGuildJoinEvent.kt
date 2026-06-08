2f351e6c851dcdc446e1d2b61754d937141032a9
acinus1001 <acinus1001@icloud.com>
2026-06-08 21:58:42 +0900

    feat: withContext 적용

--- module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/event/DiscordOnGuildJoinEvent.kt
+++ module/application/app-discord-bot/src/main/kotlin/dev/kuro9/application/discord/event/DiscordOnGuildJoinEvent.kt
@@ -3,6 +3,8 @@ package dev.kuro9.application.discord.event
 import dev.kuro9.domain.discord.bot.guilds.service.DiscordBotGuildService
 import dev.kuro9.internal.discord.model.DiscordEventHandler
 import io.github.harryjhin.slf4j.extension.info
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import net.dv8tion.jda.api.events.guild.GenericGuildEvent
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent
 import net.dv8tion.jda.api.events.guild.GuildLeaveEvent
@@ -18,20 +20,24 @@ class DiscordOnGuildJoinEvent(
         when (event) {
             is GuildJoinEvent -> {
                 info { "guild join: ${event.guild.name}" }
-                discordBotGuildService.joinGuildUpsert(
-                    botId = event.jda.selfUser.idLong,
-                    guildId = event.guild.idLong,
-                    guildName = event.guild.name,
-                    guildIconUrl = event.guild.iconUrl
-                )
+                withContext(Dispatchers.IO) {
+                    discordBotGuildService.joinGuildUpsert(
+                        botId = event.jda.selfUser.idLong,
+                        guildId = event.guild.idLong,
+                        guildName = event.guild.name,
+                        guildIconUrl = event.guild.iconUrl
+                    )
+                }
             }
 
             is GuildLeaveEvent -> {
                 info { "guild leave: ${event.guild.name}" }
-                discordBotGuildService.leaveGuild(
-                    botId = event.jda.selfUser.idLong,
-                    guildId = event.guild.idLong
-                )
+                withContext(Dispatchers.IO) {
+                    discordBotGuildService.leaveGuild(
+                        botId = event.jda.selfUser.idLong,
+                        guildId = event.guild.idLong
+                    )
+                }
             }
         }
     }
