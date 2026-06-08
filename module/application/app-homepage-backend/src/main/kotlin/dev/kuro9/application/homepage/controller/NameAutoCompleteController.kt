f0816a190e871464a4f6444d7eed7038e8501e49
acinus1001 <acinus1001@icloud.com>
2026-06-08 17:39:54 +0900

    feat: 이름 자동완성용 api

---
+++ module/application/app-homepage-backend/src/main/kotlin/dev/kuro9/application/homepage/controller/NameAutoCompleteController.kt
@@ -0,0 +1,20 @@
+package dev.kuro9.application.homepage.controller
+
+import dev.kuro9.domain.discord.name.dto.DiscordIdAndName
+import dev.kuro9.domain.discord.name.service.DiscordSearchService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/names/search")
+class NameAutoCompleteController(
+    private val discordSearchService: DiscordSearchService,
+) {
+
+    @GetMapping
+    fun searchNames(@RequestParam keyword: String): List<DiscordIdAndName> {
+        return discordSearchService.findByUsername(keyword)
+    }
+}
